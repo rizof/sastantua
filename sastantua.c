d66d52fd3ce07dba89e3c7d49d2ae1c19661abd2
rizof <kevin.cosmon@hetic.net>
2016-08-23 11:01:31 +0200

    Add files via upload

--- sastantua.c
+++ sastantua.c
@@ -8,7 +8,8 @@ int		ft_atoi(char *size)
 
 	nb = 0;
 	i = 0;
-	while (size[i] == '\t' || size[i] == '\v' || size[i] == '\r' || size[i] == '\f' || size[i] == '\n' || size[i] == ' ')
+	while (size[i] == '\t' || size[i] == '\v' || size[i] == '\r'
+		|| size[i] == '\f' || size[i] == '\n' || size[i] == ' ')
 		i++;
 	if (size[i] == '+')
 		i++;
@@ -23,97 +24,101 @@ int		ft_atoi(char *size)
 	return (nb);
 }
 
-void 	put_pyramide(int etoile, int size, int etage, int space, int boule)
-{			
-	while(etage != 0)
+void 	put_space(int etoile, int size, int etage, int size2, int indent)
+{	
+	int width;
+
+	etage = 1;
+	width = 1;
+	indent = 4;
+	while (etage <= size2)
 	{
-		while(boule < size)
-		{
-			space += 2;
-			boule++;
-		}
-		space += 4;
-		size += 1;
-		boule = 0;
-		etage--;
+		width += 2 * (2 + etage);
+		etage++;
+		width += indent;
+		if (etage % 2 && etage < size2)
+			indent += 2;
 	}
-	etoile += 1;
-	space = space / 2 - size + size - 2 ;
-	if (space >= 1)
-	{
-		while (--space)
-			write(1, " ", 1);
-	}
-	if (etoile >= 0)
-	{
-		write(1, "/", 1);
-		while (--etoile)
-			write(1, "*", 1);
-	}
+	width -= indent;
+	etage = 0;
+	while (size-- != 3)
+		etage++;
+	width = (width - etoile) / 2;
+	width -= 1;
+	while (width--)
+		write(1, " ", 1);
 }
 
-void	door(int etoile, int size, int door, int space, int boule)
-{
-	if (boule < 2)
-		put_pyramide(etoile, size, door, space, boule);
-	if (boule > 1)
+void	put_etoile(int etoile, int size, int etage, int etoile2, int boule)
+{	
+	write(1, "/", 1);
+	if (etage > 0 || boule < 2)
+		while (etoile--)
+			write(1, "*", 1);
+	if (etage == 0 && boule > 1)
 	{
 		etoile = etoile / 2;
-		etoile -= door - 1;
-		put_pyramide(etoile, size, door, space, boule);
-		door = size - 2;
-		etoile -= size - 4;
-		while (door)
-		{
-			if (door - 2 == 0 && boule == size / 2 && size > 6 && size % 2 == 0)
-				write(1, "$", 1);
-			else if (door - 2 == 0 && boule == size / 2 + 1 && size > 6)
+		etoile -= size / 2 - (+1);
+		etoile2 = etoile;
+		while (etoile--)
+			write(1, "*", 1);
+		if (size % 2 == 0)
+			etoile2 += 1;
+		etage = size - 2;
+		while (etage--)
+			if ((etage - 1 == 0 && boule == size / 2 && size > 6 && size % 2 == 0)
+				|| (etage - 1 == 0 && boule == size / 2 + 1 && size > 6))
 				write(1, "$", 1);
 			else
-				write(1,"|", 1);
-			door--;
-		}
-		while (--etoile)
+				write(1, "|", 1);
+		while (etoile2--)
 			write(1, "*", 1);
 	}
 }
 
-void 	sastantua1(int etage, int size, int etoile, int space, int boule)
+int 	calsize(int etage, int size, int etoile, int size2, int boole)
 {
-	if (etage == 0)
-		size = 0;
-	if (boule < size && etage > 1)
+	if (boole < size)
 	{
-		put_pyramide(etoile, size, etage, space, boule);
+		put_space(etoile, size, etage, size2, boole);
+		put_etoile(etoile, size, etage, size2, boole);
 		write(1, "\\", 1);
 		write(1, "\n", 1);
-		return (sastantua1(etage, size, etoile += 2, space, boule += 1));
+		return (calsize(etage, size, etoile += 2, size2, boole += 1));
 	}
-	else if (boule < size && etage == 1)
-	{
-		door(etoile, size, etage, space, boule);
-		write(1, "\\", 1);
-		write(1, "\n", 1);
-		return (sastantua1(etage, size, etoile += 2, space, boule += 1));
-	}
-	etoile -= 2;
-	if (etage > 0)
-		return (sastantua1(etage -= 1, size += 1, etoile += 6, space, boule = 0));
+	return (etoile);
+	// if (size > 4 && size % 2 == 1)
+	// 	indent += 2;
+	// if (etage > 1)
+	// 	return (calsize(etage -= 1, size += 1, etoile += indent, indent, boole = 0));
 }
 
 void 	sastantua(int size)
 {
 	int etage;
 	int etoile;
-	int space;
+	int indent;
 	int boole;
+	int size2;
 
+	size2 = size;
 	boole = 0;
 	etage = 3;
-	space = 1;
+	indent = 4;
 	etoile = 1;
-
-	sastantua1(size, etage, etoile, space, boole);
+	if (size == 0)
+		return ;
+	else
+	{
+		while (size--)
+		{
+			etoile = calsize(size, etage, etoile, size2, boole);
+			if (etage > 4 && etage % 2 == 1)
+				indent += 2;
+			etoile += indent;
+			etage += 1;
+		}
+	}
 }
 
 int		main(int argc, char **argv)
@@ -125,5 +130,5 @@ int		main(int argc, char **argv)
 		etage = ft_atoi(argv[1]);
 		sastantua(etage);
 	}
-	return 0;
+	return (0);
 }
